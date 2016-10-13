fe5ba362e7edc2eefc5b521d50ea5b462ce36148
AllenZHX <AllenZHX@github.com>
2016-10-13 17:20:00 -0400

    ubuntu, use flask!

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, json
+
+app = Flask(__name__)
+
+@app.route('/head')
+def head():
+	return render_template('head.html')
+
+@app.route('/foot')
+def foot():
+	return render_template('foot.html')
+
+
+@app.route('/fight')
+def fight():
+	return render_template('fight.html')
+
+@app.route('/twitter')
+def twitter():
+	return render_template('twitter.html')
+
+@app.route("/dictionary")
+def dictioinary():
+	return render_template('index.html')
+
+@app.route("/")
+def main():
+	return render_template('index.html')
+
+if __name__ == "__main__":
+	app.run()
