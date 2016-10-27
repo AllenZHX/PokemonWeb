925c1b46053dd74589181caf2c5389dbf019ffa8
AllenZHX <AllenZHX@github.com>
2016-10-26 23:50:37 -0400

    connect to mysql and display data on web page

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 from flask import Flask, render_template, request, json
 
+from flaskext.mysql import MySQL
+
 app = Flask(__name__)
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ',26187108hoog'
+app.config['MYSQL_DATABASE_DB'] = 'pokemon'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route('/fight')
 def fight():
 	return render_template('fight.html')
 
-@app.route('/twitter')
-def twitter():
-	return render_template('twitter.html')
-
 @app.route("/dictionary")
 def dictioinary():
 	return render_template('index.html')
+
+@app.route('/twitter')
+def show_poke():
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	query = "SELECT * FROM pokemonData WHERE Legendary='true'"
+	cursor.execute(query);
+    	poke = cursor.fetchall()
+    	return render_template('twitter.html', poke=poke)
 
 @app.route("/")
 def main():
