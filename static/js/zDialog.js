764ea346f8a459ab3a876822b5bd7fa9eb1b215c
AllenZHX <AllenZHX@github.com>
2016-11-06 11:03:45 -0500

    done with bug of static's url

--- static/js/zDialog.js
+++ static/js/zDialog.js
@@ -1,8 +1,8 @@
-﻿/**
+/**
  * zDialog 2.0
  * 最后修正：2009-12-18
  **/
-var IMAGESPATH = '../images/'; //图片路径配置
+var IMAGESPATH = '/images/'; //图片路径配置
 /*************************一些公用方法和属性****************************/
 var isIE = navigator.userAgent.indexOf('MSIE') != -1;
 var isIE6 = navigator.userAgent.indexOf('MSIE 6.0') != -1;
