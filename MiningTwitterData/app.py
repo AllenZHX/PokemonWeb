7f05bc08d6d356f2386fcd15f71523f512d8c9af
Xiang Cao <xcao4@stevens.edu>
2016-11-10 01:53:20 -0500

    site works well locally

--- MiningTwitterData/app.py
+++ MiningTwitterData/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from pymongo import MongoClient
 
-client = MongoClient()
-db = client.twitter_DB
-collection = db.twitter_Collection
+client = MongoClient("mongodb://xiang:1@ds149437.mlab.com:49437/twitter_db")
+db = client.twitter_db
+collection = db.twitter_collection
 
 app = Flask(__name__)
 
@@ -12,22 +12,21 @@ count=0
 for tweet in tweets_iterator:
     count=count+1
     print tweet['text'],count
-    print
+    print   "***********"
 
 f = open('top3','wb')
 f.write(str(count)) # python will convert \n to os.linesep
 f.close() # you can omit in most cases as the destructor will call it
 
-# @app.route('/pokemon',methods=['POST','PUT','GET'])
-# def getAllPokemon():
-#     # tweets_iterator = collection.find({'text': {'$regex': 'Mewtwo'}}).limit(10)
-#     tweets_iterator=collection.find().limit(5)
-#     # res = ''
-#     for tweet in tweets_iterator:
-#         print tweet['text']
-#         # res = res+tweet['text']
-#
-#     # return res
-#
-# if __name__ == '__main__':
-#     app.run(debug=True)
+@app.route('/',methods=['POST','PUT','GET'])
+def getAllPokemon():
+    res=""
+    tweets_iterator = collection.find({'text': {'$regex': 'Pikachu'}}).limit(10)
+    # tweets_iterator=collection.find().limit(5)
+    for tweet in tweets_iterator:
+        # print tweet['text']
+        res =res+(tweet['text'])+">>>>>>>>>>>>>>>>"
+    return res
+
+if __name__ == '__main__':
+    app.run()
